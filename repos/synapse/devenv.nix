c50625776b9e3ad37c8b33e7ebf5fa64b16dfcb0
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-06-26 19:03:55 +0000

    fix: remove rust deps from synapse

--- repos/synapse/devenv.nix
+++ repos/synapse/devenv.nix
@@ -5,25 +5,24 @@
   inputs,
   ...
 }: {
-  packages = [
-    # Requires ffmpeg4 because of this https://github.com/gcanat/video_reader-rs/issues/4
-    pkgs.ffmpeg_4.dev
-    pkgs.llvmPackages_latest.clang # your C compiler
-    pkgs.llvmPackages_latest.libclang # for bindgen
-    pkgs.pkg-config # to find libavutil.pc
-    pkgs.glibc.dev # <-- brings in stdint.h, etc.
-    pkgs.zlib.dev # <-- bring in other common headers
-  ];
-  env.DECORD_REWIND_RETRY_MAX = "64";
+  # packages = [
+  #   # Requires ffmpeg4 because of this https://github.com/gcanat/video_reader-rs/issues/4
+  #   pkgs.ffmpeg_4.dev
+  #   pkgs.llvmPackages_latest.clang # your C compiler
+  #   pkgs.llvmPackages_latest.libclang # for bindgen
+  #   pkgs.pkg-config # to find libavutil.pc
+  #   pkgs.glibc.dev # <-- brings in stdint.h, etc.
+  #   pkgs.zlib.dev # <-- bring in other common headers
+  # ];
   env.BUILDX_BAKE_ENTITLEMENTS_FS = "0";
   # https://discourse.nixos.org/t/libclang-path-and-rust-bindgen-in-nixpkgs-unstable/13264
-  env.LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+  # env.LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
 
   # env.LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.ffmpeg];
 
-  languages.rust = {
-    enable = true;
-  };
+  # languages.rust = {
+  #   enable = true;
+  # };
   languages.python = {
     libraries = [
       # Otherwise pandas yells at us for not being able to find libz.so.1
