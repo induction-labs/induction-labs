4fc75ee6fcc3d74b1d1cc7c1248c1e0aafd97b1f
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-07-12 00:45:37 +0000

    disable cudnn benchmarking

--- repos/modeling/src/modeling/initialization.py
+++ repos/modeling/src/modeling/initialization.py
@@ -272,14 +272,16 @@ class ExperimentInstance:
         os.environ["CUBLAS_WORKSPACE_CONFIG"] = (
             ":4096:8"  # for reproducibility in cuBLAS
         )
+
         # https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
         random.seed(seed)  # Python RNG
         np.random.seed(seed)  # NumPy RNG
         torch.manual_seed(seed)  # CPU RNG
         torch.cuda.manual_seed(seed)  # current GPU RNG
         torch.cuda.manual_seed_all(seed)  # all-GPU RNGs
-        # torch.backends.cudnn.deterministic = True
-        # torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
         # # In PyTorch ≥1.8 you can also do:
         torch.use_deterministic_algorithms(True)
         g = torch.Generator()
