0cd8ddf5224bccca20a38ac415491613b415403e
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-07-22 22:23:52 +0000

    fix: datapack things

--- repos/modeling/src/modeling/config/serde.py
+++ repos/modeling/src/modeling/config/serde.py
@@ -29,18 +29,28 @@ def build_experiment_config(experiment_config_toml: Path) -> ExperimentConfig[An
         serialized_exp_config.module.model_dump(serialize_as_any=True)
     )
 
-    datapack_config_path = serialized_exp_config.datapack.config_path
-    datapack_config_cls = import_from_string(datapack_config_path)
-    assert issubclass(datapack_config_cls, DatapackConfig)
-    datapack_config = datapack_config_cls.model_validate(
-        serialized_exp_config.datapack.model_dump(serialize_as_any=True)
+    train_datapack_config_path = serialized_exp_config.train_datapack.config_path
+    train_datapack_config_cls = import_from_string(train_datapack_config_path)
+    assert issubclass(train_datapack_config_cls, DatapackConfig)
+    train_datapack_config = train_datapack_config_cls.model_validate(
+        serialized_exp_config.train_datapack.model_dump(serialize_as_any=True)
+    )
+
+    validation_datapack_config_path = (
+        serialized_exp_config.validation_datapack.config_path
+    )
+    validation_datapack_config_cls = import_from_string(validation_datapack_config_path)
+    assert issubclass(validation_datapack_config_cls, DatapackConfig)
+    validation_datapack_config = validation_datapack_config_cls.model_validate(
+        serialized_exp_config.validation_datapack.model_dump(serialize_as_any=True)
     )
 
     return ExperimentConfig.model_validate(
         {
             **serialized_exp_config.model_dump(),
             "module": module_config,
-            "datapack": datapack_config,
+            "train_datapack": train_datapack_config,
+            "validation_datapack": validation_datapack_config,
         }
     )
 
